33ca5ed7b8502dc4548bb40e8411a714cd0ba6b5
Jim Rauch <jim@autalic.ai>
2025-08-18 14:11:55 +1200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    if (!config.externals) config.externals = [];
+    config.externals.push('googleapis'); // prevent Next.js from bundling it
+    return config;
+  },
 };
 
 module.exports = nextConfig;
