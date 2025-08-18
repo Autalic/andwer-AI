f294d8dd20df6a71cbb8f959c673597a8180a971
Jim Rauch <jim@autalic.ai>
2025-08-18 14:50:09 +1200

    Update telnyx.js

--- api/telnyx.js
+++ api/telnyx.js
@@ -22,9 +22,10 @@ export default async function handler(req, res) {
       auth: {
         type: "OAuth2",
         user: process.env.GMAIL_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+clientId: process.env.GMAIL_CLIENT_ID,
+clientSecret: process.env.GMAIL_CLIENT_SECRET,
+refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+
         accessToken,
       },
     });
